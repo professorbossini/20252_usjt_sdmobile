317f6d6d972e34ff30455b22f89b57a83d8e578b
Rodrigo Bossini <professorbossini@gmail.com>
2025-08-25 19:39:07 -0300

    learn(ap01-js): encerra secao 1.6.2

---
+++ js/script.js
@@ -0,0 +1,102 @@
+
+
+// //funções
+// //functions
+// //arrow functions
+// const triplo = function(n = 5){
+//   return 3 * n
+// }
+// console.log(triplo(10))
+// console.log(triplo())
+// const dobro = function(n){
+//   return 2 * n
+// }
+// console.log(dobro(5))
+// function somar(a, b){
+//   return a + b
+// }
+// const res = somar(2, 3)
+// console.log(res)
+// function hello(){
+//   console.log('Hello')
+// }
+// hello()
+
+// function hello(nome){
+//   console.log(`Hello, ${nome}`)  
+// }
+// hello('Ana')
+
+//vetores, listas, coleções
+// v2 = [2, "abc", true]
+// for(let i = 0; i < v2.length; i++)
+//   console.log(v2[i])
+// v1 = []
+// console.log(v1.length)
+// v1[0] = 5
+// console.log(v1)
+// console.log(v1.length)
+// v1[10] = "abc"
+// console.log(v1.length)
+// console.log(v1)
+//comparação
+//== ou ===
+// console.log([] == [])
+// console.log([] == false)
+//null: representa a inexistência de um objeto
+//undefined: não está definido
+// console.log(null == undefined)
+// console.log(null == null)
+// console.log([1] == 1)
+// console.log(true == 1)
+// console.log(1 === '1')
+// console.log(1 === 1)
+// console.log(1 == '1')
+// console.log(1 == 1)
+//coerção
+// const n1 = 2
+// const n2 = '3'
+// const n3 = n1 + n2
+// console.log(n3)
+// const n4 = n1 + Number(n2)
+// console.log(n4)
+
+//tipos
+// lista = []
+// List <String> lista = new ArrayList<>();
+// let nome = "Ana"
+// console.log(typeof(nome))
+// nome = 2
+// console.log(typeof(nome))
+// nome.falar()
+// String nome = "Ana";
+// nome = 1;
+// nome.falar();
+//declarando variáveis e constantes
+// let idade = 19
+// console.log(`Oi, ${nome}`)
+// if(idade >= 18){
+//   //hoist: içamento
+//   let nome = 'João'
+//   console.log(`${nome} pode dirigir`)
+// }
+// console.log(`Até mais, ${nome}`)
+// var linguagem = 'Javascript'
+// console.log(`Aprendendo ${linguagem}`)
+// var linguagem = 'Java'
+// console.log(`Aprendendo ${linguagem}`)
+// var c = 2
+// console.log(c)
+// c = 3
+// console.log(c)
+// let a = 2
+// a = a + 1
+// console.log(a)
+//var, const e let
+// const nome = 'José'
+// const sobrenome = "Silva"
+// //interpolação
+// console.log(`${nome} ${sobrenome}`)
+//concatenação
+// console.log(nome + " " + sobrenome)
+//nome = 'José da Silva'
