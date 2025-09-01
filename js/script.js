defdd449c1434aee5cb6899c4a9fdf6ba759cf50
Rodrigo Bossini <professorbossini@gmail.com>
2025-09-01 19:16:42 -0300

    learn(ap01-js): encerra capitulo 1
    - retomar falando sobre json

--- js/script.js
+++ js/script.js
@@ -1,4 +1,105 @@
+// function eAgora(){
+//   let cont = 1
+//   function f1(){
+//     console.log(++cont)
+//   }
+//   cont++
+//   function f2(){
+//     console.log(cont++)
+//     return cont
+//   }
+//   cont++
+//   return {f1, f2}
+// }
 
+// const res = eAgora()
+// res.f1()
+// console.log(res.f2())
+
+// function saudacoesFactory(saudacao, nome){
+//   return function(){
+//     console.log(`${saudacao}, ${nome}`)
+//   }
+// }
+
+// const oiJoao = saudacoesFactory('Oi', 'João')
+// const tchauJoao = saudacoesFactory('Tchau', 'João')
+// oiJoao()
+// tchauJoao()
+
+
+// function f(){
+//   let nome = 'João'
+//   function g(){
+//     console.log(nome)
+//   }
+//   return g
+// }
+// const resultadoDaF = f()
+// resultadoDaF()
+
+// function f(funcao){
+//   console.log('f')
+//   return funcao()
+// }
+
+// function g(){
+//   console.log('g')
+//   function outraFuncao(){
+//     console.log('Fui criada por g')
+//     return () => "a"
+//   }
+//   return outraFuncao
+// }
+// console.log(f(g)()())
+
+// //closures
+// let umaFuncao = function(){
+//   console.log('Fui armazenada em uma variável')
+//   return () => console.log('oi')
+// }
+// umaFuncao()()
+
+// function f(funcao){
+//   console.log(funcao)
+//   funcao()
+// }
+// f(umaFuncao())
+
+// class Pessoa{
+//   public void andar(){
+
+//   }
+//   var teste = andar;
+// }
+// const a = 1
+// const f = b => b + 1
+// f(1)
+// const f2 = () => 1
+
+//arrow functions
+// const ehPar = n => {
+//   n % 2 === 0
+// }
+// console.log(ehPar(5))
+// const triplo = (n) => {
+//   console.log(n)
+//   return 3 * n
+// }
+// console.log(triplo(10))
+// const dobro = v => v * 2
+// console.log(dobro(5))
+// const f5 = a => 1
+// console.log(f5())
+// const f4 = (a, b) => a + b
+// console.log(f4(2, 3))
+// const f3 = a => console.log(a)
+// f3(1)
+// const f2 = () => console.log('oi')
+// f2()
+// const f1 = a => {console.log(a)}
+// f1(1)
+// () => {}
 
 // //funções
 // //functions
