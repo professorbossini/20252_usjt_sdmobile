7a2d7918ab59106910eb3a6abd0dfdb1f81e984e
Rodrigo Bossini <professorbossini@gmail.com>
2025-09-15 21:59:46 -0300

    learn(ap01-js): encerra a apostila
    - retomar lidando com mss

--- condicoes_climaticas/index.js
+++ condicoes_climaticas/index.js
@@ -12,10 +12,54 @@ const {
 
 const url = `${PROTOCOL}://${BASE_URL}?appid=${APPID}&q=${Q}&cnt=${CNT}&units=${UNITS}&lang=${LANG}`
 
-axios.get(url)
-.then((res) => {
-  console.log(res.data)
-  console.log('Exibindo o dt')
-  console.log(new Date(Number(res.data.list[0].dt) * 1000))
-  console.log(new Date(Number(res.data.list[1].dt) * 1000))      
-})
+async function comAsyncAwait(){
+  try{
+    console.log('************************')
+    const result = await axios.get(url)
+    console.log(result)
+    console.log('************************')
+    console.log('Agora o data')
+    console.log(result.data)
+    console.log('************************')
+    console.log('Agora o list')
+    console.log(result.data.list)
+    //iterar sobre a lista, mostrando a sensação térmica de cada previsão
+    //ah, mostrar o nível do mar tb
+  }
+  catch(e){
+    console.log(e)
+  }
+}
+comAsyncAwait()
+
+
+
+// const teste = async() => {
+//   return 1
+// }
+// // console.log(teste())
+// teste().then(res => console.log(res))
+
+
+// axios.get(url)
+// .then((res) => {
+//   console.log('Exibindo o corpo da resposta...')
+//   console.log(res.data)
+//   console.log('*************************')
+//   return res.data.list
+// })
+// .then((res) => {
+//   console.log('Exibindo a lista inteira')
+//   console.log(res)
+//   console.log('*************************')
+//   return res
+// })
+// .then((res) => {
+//   console.log('Iterando sobre a lista')
+//   for(let previsao of res){
+//     //data
+//     console.log(`dt: ${new Date(previsao.dt * 1000).toLocaleString()}`)
+//     console.log(`Temp min: ${previsao.main.temp_min}`)
+//     console.log(`Descrição: ${previsao.weather[0].description}`)
+//   }
+// })
